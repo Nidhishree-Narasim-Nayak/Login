7fe07991b7620ad797c95cff309ad00722bde7d8
Nidhishree-Narasim-Nayak <nayaknidhishree06@gmail.com>
2025-07-02 21:15:32 +0530

    Login Screen Completed

--- src/screen/LoginScreen.tsx
+++ src/screen/LoginScreen.tsx
@@ -1,14 +1,185 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  Image,
+} from 'react-native';
+import React, { useState } from 'react';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
+import { colors } from '../utils/colors';
+import { fonts } from '../utils/fonts';
+import { useNavigation } from '@react-navigation/native';
 
 const LoginScreen = () => {
+  const navigation = useNavigation();
+  const [secureEntry, setSecureEntry] = useState(true);
+  const handleGoBack = () => {
+    navigation.goBack();
+  };
   return (
-    <View>
-      <Text>LoginScreen</Text>
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.backButtonWrapper} onPress={handleGoBack}>
+        <Ionicons
+          name={'arrow-back-outline'}
+          color={colors.primary}
+          size={25}
+        />
+      </TouchableOpacity>
+      <View style={styles.textContainer}>
+        <Text style={styles.headingText}>Hey,</Text>
+        <Text style={styles.headingText}>Welcome</Text>
+        <Text style={styles.headingText}>Back</Text>
+        <View style={styles.formContainer}>
+          <View style={styles.inputConatiner}>
+            <Ionicons name="mail-outline" size={25} color={colors.secondary} />
+            <TextInput
+              style={styles.textInput}
+              placeholder="Enter your email"
+              placeholderTextColor={colors.secondary}
+              keyboardType="email-address"
+            />
+          </View>
+          <View style={styles.inputConatiner}>
+            <SimpleLineIcons name="lock" size={25} color={colors.secondary} />
+            <TextInput
+              style={styles.textInput}
+              placeholder="Enter your passwordddd"
+              placeholderTextColor={colors.secondary}
+              secureTextEntry={secureEntry}
+            />
+            <TouchableOpacity
+              onPress={() => {
+                setSecureEntry(prev => !prev);
+              }}
+            >
+              <SimpleLineIcons name="eye" size={20} color={colors.secondary} />
+            </TouchableOpacity>
+          </View>
+          <TouchableOpacity>
+            <Text style={styles.forgotPasswordText}>Forgot Password?</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.loginButtonWrapper}>
+            <Text style={styles.loginText}>Login</Text>
+          </TouchableOpacity>
+          <Text style={styles.continueText}>or continue with</Text>
+          <TouchableOpacity style={styles.googleButtonContainer}>
+            <Image
+              source={require('../assets/google.png')}
+              style={styles.googleImage}
+            />
+            <Text style={styles.googleText}>Google</Text>
+          </TouchableOpacity>
+          <View style={styles.footerContainer}>
+            <Text style={styles.accountText}>Don't have an account?</Text>
+            <Text style={styles.signupText}>Sign up</Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
 
 export default LoginScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.white,
+    marginTop: 50,
+    padding: 20,
+  },
+  backButtonWrapper: {
+    height: 40,
+    width: 40,
+    backgroundColor: colors.gray,
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textContainer: {
+    marginVertical: 20,
+  },
+  headingText: {
+    fontSize: 32,
+    color: colors.primary,
+    fontFamily: fonts.SemiBold,
+  },
+  formContainer: {
+    marginTop: 20,
+  },
+  inputConatiner: {
+    borderWidth: 1,
+    borderColor: colors.secondary,
+    borderRadius: 100,
+    paddingHorizontal: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+    marginVertical: 10,
+  },
+  textInput: {
+    flex: 1,
+    paddingHorizontal: 10,
+    fontFamily: fonts.Light,
+  },
+  forgotPasswordText: {
+    textAlign: 'right',
+    color: colors.primary,
+    fontFamily: fonts.SemiBold,
+    marginVertical: 10,
+  },
+  loginButtonWrapper: {
+    backgroundColor: colors.primary,
+    borderRadius: 100,
+    marginTop: 20,
+  },
+  loginText: {
+    color: colors.white,
+    fontSize: 20,
+    fontFamily: fonts.SemiBold,
+    textAlign: 'center',
+    padding: 10,
+  },
+  continueText: {
+    textAlign: 'center',
+    marginVertical: 20,
+    fontSize: 14,
+    fontFamily: fonts.Regular,
+    color: colors.primary,
+  },
+  googleButtonContainer: {
+    flexDirection: 'row',
+    borderWidth: 2,
+    borderColor: colors.primary,
+    borderRadius: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+    gap: 10,
+  },
+  googleImage: {
+    height: 20,
+    width: 20,
+  },
+  googleText: {
+    fontSize: 20,
+    fontFamily: fonts.SemiBold,
+  },
+  footerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 20,
+    gap: 2,
+  },
+  accountText: {
+    color: colors.primary,
+    fontFamily: fonts.Regular,
+  },
+  signupText: {
+    fontFamily: fonts.Bold,
+  },
+});
